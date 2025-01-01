d360a0082071345c4c108558c59952b714da6e92
almusavveerjamadar <almusavveerjamadar@gmail.com>
2025-01-01 12:35:03 +0000

    Header Section done

---
+++ src/assets/images.js
@@ -0,0 +1,13 @@
+import logo from "../assets/images/xora.svg";
+import magic from "../assets/images/magic.svg";
+import close from "../assets/images/close.svg";
+import outline from '../assets/images/bg-outlines.svg'
+import outline_fill from '../assets/images/bg-outlines-fill.png'
+
+export const images = {
+  logo,
+  magic,
+  close,
+  outline, 
+  outline_fill
+};
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, { useState } from "react";
+import { images } from "../assets/images.js";
+import { Link as LinkScroll } from "react-scroll";
+import clsx from "clsx";
+
+const NavLink = ({ title }) => (
+  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-100 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+    {title}
+  </LinkScroll>
+);
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>Header</div>
-  )
-}
+    <header className="fixed top-0 left-0 z-50 w-full py-10">
+      <div className="container flex h-14 items-center max-lg:px-5">
+        <a className="lg:hidden flex-1 cursor-pointer z-2">
+          <img src={images.logo} alt="logo" width={115} height={55} />
+        </a>
 
-export default Header
+        {/* {clsx(
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+            isOpen ? "max-lg:opacity-0" : "max-lg:pointer-events-none "
+          )} */}
+        <div
+          className={clsx(
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
+          )}
+        >
+          <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:overflow-hidden sidebar-before max-md:px-4">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+              <ul className="flex max-lg:block max-lg:px-12">
+                <li className="nav-li">
+                  <NavLink title="features" />
+                  <div className="dot" />
+                  <NavLink title="pricing" />
+                </li>
+
+                <li className="nav-logo">
+                  <LinkScroll
+                    to="hero"
+                    offset={-100}
+                    spy
+                    smooth
+                    className={clsx(
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer"
+                    )}
+                  >
+                    <img src={images.logo} alt="logo" width={160} height={55} />
+                  </LinkScroll>
+                </li>
+
+                <li className="nav-li">
+                  <NavLink title="faq" />
+                  <div className="dot" />
+                  <NavLink title="download" />
+                </li>
+              </ul>
+            </nav>
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+              <img
+                src={images.outline}
+                alt="outline"
+                width={960}
+                height={380}
+                className="relative z-2 opacity-[0.6]"
+              />
+              <img
+                src={images.outline_fill}
+                alt="outline"
+                width={960}
+                height={380}
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
+            </div>
+          </div>
+        </div>
+
+        <button
+          className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <img
+            src={isOpen ? images.close : images.magic}
+            alt="magic"
+            className="size-1/2 object-contain"
+          />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
