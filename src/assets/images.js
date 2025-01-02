f4f7db42aa4cff21dd942cff88fb7bb87b74cb15
almusavveerjamadar <almusavveerjamadar@gmail.com>
2025-01-02 11:56:00 +0000

    Features Section Done

--- src/assets/images.js
+++ src/assets/images.js
@@ -5,6 +5,14 @@ import outline from "../assets/images/bg-outlines.svg";
 import outline_fill from "../assets/images/bg-outlines-fill.png";
 import zap from "../assets/images/zap.svg";
 import hero from "../assets/images/hero.png";
+import feature1 from "../assets/images/feature-1.png";
+import feature2 from "../assets/images/feature-2.png";
+import magictouch from "../assets/images/magictouch.svg";
+import doc from "../assets/images/docs.svg";
+import detatil1 from "../assets/images/detail-1.png";
+import detatil2 from "../assets/images/detail-2.png";
+import detatil3 from "../assets/images/detail-3.png";
+import detatil4 from "../assets/images/detail-4.png";
 
 export const images = {
   logo,
@@ -14,4 +22,12 @@ export const images = {
   outline_fill,
   zap,
   hero,
+  feature1,
+  feature2,
+  magictouch,
+  doc,
+  detatil1,
+  detatil2,
+  detatil3,
+  detatil4,
 };
