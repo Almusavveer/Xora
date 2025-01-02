61ec7dea8661616c4a6904f55390b0b098afeee6
almusavveerjamadar <almusavveerjamadar@gmail.com>
2025-01-02 09:11:13 +0000

    Hero section done

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,13 +1,17 @@
 import logo from "../assets/images/xora.svg";
 import magic from "../assets/images/magic.svg";
 import close from "../assets/images/close.svg";
-import outline from '../assets/images/bg-outlines.svg'
-import outline_fill from '../assets/images/bg-outlines-fill.png'
+import outline from "../assets/images/bg-outlines.svg";
+import outline_fill from "../assets/images/bg-outlines-fill.png";
+import zap from "../assets/images/zap.svg";
+import hero from "../assets/images/hero.png";
 
 export const images = {
   logo,
   magic,
   close,
-  outline, 
-  outline_fill
+  outline,
+  outline_fill,
+  zap,
+  hero,
 };
